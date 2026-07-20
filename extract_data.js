ac706c81cec7502f986c209a3dd1919ad928dea3
Piotr Kaminski <piotr@reviewable.io>
2026-07-20 15:43:19 -0700

    Track source ghost account separately

--- extract_data.js
+++ extract_data.js
@@ -15,7 +15,7 @@ import {uploadedFilesUrl, PLACEHOLDER_URL} from './lib/derivedInfo.js';
 
 const NodeFire = nodefireModule.default;
 
-const TARGETS = ['saas', 'ghes'];
+const HOSTING_ENVIRONMENTS = ['saas', 'ghes'];
 
 const commandLineOptions = [
   {name: 'repos', alias: 'r', typeLabel: '{underline repos.json}',
@@ -29,9 +29,23 @@ const commandLineOptions = [
   {name: 'merge', alias: 'm', type: Boolean,
     description: 'Extract data in a format suitable for merging users into an existing instance'},
   {
+    name: 'source',
+    type: value => {
+      if (!_.includes(HOSTING_ENVIRONMENTS, value)) {
+        throw new Error(`Expected one of: ${HOSTING_ENVIRONMENTS.join(', ')}`);
+      }
+      return value;
+    },
+    defaultValue: 'saas',
+    typeLabel: '{underline saas|ghes}',
+    description: 'Source hosting environment. (Optional, defaults to saas.)'
+  },
+  {
     name: 'target',
     type: value => {
-      if (!_.includes(TARGETS, value)) throw new Error(`Expected one of: ${TARGETS.join(', ')}`);
+      if (!_.includes(HOSTING_ENVIRONMENTS, value)) {
+        throw new Error(`Expected one of: ${HOSTING_ENVIRONMENTS.join(', ')}`);
+      }
       return value;
     },
     defaultValue: 'ghes',
@@ -89,7 +103,8 @@ const repoNames =
   _(args.repos).thru(fs.readFileSync).thru(JSON.parse).map(_.toLower).uniq().value();
 const repoNamesSet = new Set(repoNames);
 const orgNames = _(repoNames).map(name => name.replace(/\/.*/, '')).uniq().value();
-const ghostUserKey = args.target === 'saas' ? 'github:10137' : 'github:1';
+const sourceGhostUserKey = args.source === 'saas' ? 'github:10137' : 'github:1';
+const targetGhostUserKey = args.target === 'saas' ? 'github:10137' : 'github:1';
 
 const out = new PromiseWritable(fs.createWriteStream(args.output));
 out.stream.setMaxListeners(Infinity);
@@ -385,7 +400,7 @@ async function extractUsers() {
   if (_.isEmpty(userMap)) return;
   log('Extracting users');
   await forEachOfLimit(userMap, 25, async (newUserKey, oldUserKey) => {
-    if (newUserKey === ghostUserKey) return;
+    if (newUserKey === targetGhostUserKey) return;
     let user = await db.child('users/:oldUserKey', {oldUserKey}).get();
     if (!user) {
       unknownUsers.push(oldUserKey);
@@ -473,8 +488,8 @@ function mapUserKey(userKey, context) {
     userMap[userKey] = userKey;
     pace.total += 1;
   }
-  const newUserKey = userMap[userKey] || ghostUserKey;
-  if (!userMap[userKey] && userKey !== ghostUserKey) ghostedUsers.push({userKey, context});
+  const newUserKey = userMap[userKey] || targetGhostUserKey;
+  if (!userMap[userKey] && userKey !== sourceGhostUserKey) ghostedUsers.push({userKey, context});
   return newUserKey;
 }
 
